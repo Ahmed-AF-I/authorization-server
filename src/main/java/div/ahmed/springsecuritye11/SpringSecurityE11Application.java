368579ff482e39f519008eae68c03a4ab0c2c034
Ahmed-AF-I <ahmed.405xy@gmail.com>
2026-08-06 06:45:02 -0400

    unlock the user authorities

--- src/main/java/div/ahmed/springsecuritye11/SpringSecurityE11Application.java
+++ src/main/java/div/ahmed/springsecuritye11/SpringSecurityE11Application.java
@@ -2,7 +2,7 @@ package div.ahmed.springsecuritye11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// http://localhost:8080/oauth2/authorize?response_type=code&client_id=client&scope=openid&redirect_uri=https://springone.io/authorized
 @SpringBootApplication
 public class SpringSecurityE11Application {
 
